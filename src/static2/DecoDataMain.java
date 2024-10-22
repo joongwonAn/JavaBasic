c89b4751ad880daa6fe1d475737051bf618bfc5d
joongwon <sara970517@gmail.com>
2024-10-22 19:38:48 +0900

    8. 자바 메모리 구조와 static - static 메서드 사용

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,16 @@
 package static2;
 
+//import static static2.DecoData.*;
+//import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
+        /* static import
+         * 정적 메서드를 사용할 때 해당 메서드를 아래와 같이 자주 호출해야 한다며 static import 기능을 고려하자
+         * staticCall(); 로 호출이 가능해진다.
+        */
         DecoData.staticCall();
 
         System.out.println("2. 인스턴스 호출1");
