e8d16886f414ba4d53bfec5c1e1948ff2b027b3c
joongwon <sara970517@gmail.com>
2024-10-22 20:12:15 +0900

    8. 자바 메모리 구조와 static - 문제1) 구매한 자동차 수

--- src/static2/ex/Car.java
+++ src/static2/ex/Car.java
@@ -1,11 +1,13 @@
 package static2.ex;
 
 public class Car {
+    // 지금은 사용하지 않지만 보통 이름을 전달해주면 Car 객체의 속성을 보관해두는 것이 일반적이기 때문에 name 필드를 추가한 것
     private String name;
     private static int count;
 
     public Car(String name) {
         count++;
+        this.name = name;
         System.out.println("차량구입, 이름: " + name);
     }
 
