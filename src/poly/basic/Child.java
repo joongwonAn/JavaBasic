e69b666eab279780ceccc1bab492de05b42762aa
joongwon <sara970517@gmail.com>
2024-10-30 13:18:49 +0900

    10. 다형성1

--- src/poly/basic/Child.java
+++ src/poly/basic/Child.java
@@ -5,4 +5,5 @@ public class Child extends Parent {
     public void childMethod() {
         System.out.println("Child Method");
     }
+
 }
--- src/poly/basic/Parent.java
+++ src/poly/basic/Parent.java
@@ -5,4 +5,5 @@ public class Parent {
     public void parentMethod() {
         System.out.println("Parent Method");
     }
+
 }
